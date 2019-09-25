7442016d4ec9cc4842a06a22f8887e45bdcbffdb
Bao Vuong <vqbao@lv5.org>
2019-09-25 02:41:17 -0400

    Add project files.

---
+++ GimmeTheCopyPasta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GimmeTheCopyPasta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
