7e6b7cd225cdfa89cd7b81f436f71995531bb4b0
Bao Vuong <vqbao@lv5.org>
2019-09-28 19:24:57 -0400

    working through things

--- GimmeTheCopyPasta/Program.cs
+++ GimmeTheCopyPasta/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Reddit;
 
 namespace GimmeTheCopyPasta
 {
@@ -6,7 +9,24 @@ namespace GimmeTheCopyPasta
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            
+
+            var secret = config["secret"];
+            var appId = config["appId"];
+
+            var reddit = new RedditAPI(appId: appId, appSecret: secret);
+
+            var results = reddit.Subreddit("copypasta").Posts.Best;
+            foreach (var result in results)
+            {
+                Console.WriteLine(result.Title);
+            }
         }
     }
 }
